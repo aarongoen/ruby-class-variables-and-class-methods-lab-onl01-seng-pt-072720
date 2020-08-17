dae3ad1783710572d5326b085e60a7c0947d1a1d
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-17 20:32:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@artists = [] 
+   
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+   
+    @@count = count 
+    
+    @@genres = [] 
+
+    @@artists = []
+
+    # @@artist_count = {}
+    # @@genre_count = {}
+  end
+  
+  def count
+    @@name.count  
+  end 
+  
+  def artists=(artist)
+    @artist = artist
+      @@artists << artist 
+    end 
+  end 
+  
+  def genres 
+    @genre = genre
+    if !@@genres.include?(genre)
+      @@genres << genres
+    end 
+  end 
+  
+  def genre_count
+    @@genres << @genre
+    @@genres.count
+  end 
+  
+#   def self.artist_count
+#     Song.map do |name, artist|
+#     Song[artist] = name.count 
+#     end
+#     Song 
+#   end 
+end 
