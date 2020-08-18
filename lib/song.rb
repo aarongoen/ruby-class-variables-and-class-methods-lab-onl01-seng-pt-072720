55aa6362555341c347e6672b3459f1136a59d3eb
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-18 00:51:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,7 @@ class Song
   def self.artist_count
     artist_hash = {}
     @@artists.each do |artist|
-      if @artist_hash.has_key?(artist)
+      if artist_hash.has_key?(artist)
         artist_hash[artist] += 1
       else 
         artist_hash[artist] = 1   
